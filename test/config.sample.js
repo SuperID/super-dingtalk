49d819626fec66347e98c4c6e8bd066ddc4480ce
YourtionGuo <yourtion@gmail.com>
2016-11-22 14:59:41 +0800

    add test and config sample

---
+++ test/config.sample.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  corpId: 'xxxxxxx',
+  secret: 'xxxxxxx',
+  department_id: '123456',
+};
--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 
 const config = require('./config');
 const DingTalk = require('../lib');
+const should = require('should');
 
 const dingTalk = new DingTalk({
   corpId: config.corpId,
@@ -10,15 +11,39 @@ const dingTalk = new DingTalk({
 
 describe('DingTalk', function () {
 
-  it('access token', function (done) {
+  it('get token', function (done) {
     dingTalk.getToken()
     .then(ret => {
-        console.log('success', ret);
-        done();
+      should.ok(ret);
+      done();
     })
     .catch(err => {
-        console.log('error', err);
-        done(err);
+      console.log('error', err);
+      done(err);
+    });
+  });
+
+  it('get ticket', function (done) {
+    dingTalk.getTicket()
+    .then(ret => {
+      should.ok(ret);
+      done();
+    })
+    .catch(err => {
+      console.log('error', err);
+      done(err);
+    });
+  });
+
+  it('simpleList', function (done) {
+    dingTalk.simpleList(config.department_id)
+    .then(ret => {
+      should.ok(ret);
+      done();
+    })
+    .catch(err => {
+      console.log('error', err);
+      done(err);
     });
   });
 
